c2b606e36d647699759ecc5c44aec0f58522fc40
Ismael Garcia Pascual~ <garciapismael@gmail.com>
2022-04-01 12:18:58 +0200

    home 3 commit

--- lib/src/ui/pages/home/home_page.dart
+++ lib/src/ui/pages/home/home_page.dart
@@ -10,8 +10,66 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<HomeController>(
       create: (_) => HomeController(),
-      builder: (_, __) => Scaffold(
-        bottomNavigationBar: HomeBottomBar(),
+      builder: (_, __) => DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              "logo",
+              style: TextStyle(
+                color: Colors.black,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            backgroundColor: Colors.white,
+            bottom: TabBar(
+              indicatorColor: Colors.redAccent,
+              indicatorWeight: 3,
+              labelColor: Colors.redAccent,
+              unselectedLabelColor: Colors.black38,
+              indicator: BoxDecoration(
+                color: Colors.white,
+                shape: BoxShape.circle,
+                border: Border.all(width: 1, color: Colors.redAccent),
+              ),
+              tabs: const [
+                Tab(
+                  icon: Icon(Icons.home),
+                ),
+                Tab(
+                  icon: Icon(Icons.favorite),
+                ),
+                Tab(
+                  icon: Icon(Icons.notifications),
+                ),
+                Tab(
+                  icon: Icon(Icons.person),
+                ),
+              ],
+            ),
+          ),
+          //bottomNavigationBar: HomeBottomBar(),
+          body: TabBarView(
+            children: [
+              Container(
+                child: const Text("home"),
+                alignment: Alignment.center,
+              ),
+              Container(
+                child: const Text("favorites"),
+                alignment: Alignment.center,
+              ),
+              Container(
+                child: const Text("notifications"),
+                alignment: Alignment.center,
+              ),
+              Container(
+                child: const Text("more"),
+                alignment: Alignment.center,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
